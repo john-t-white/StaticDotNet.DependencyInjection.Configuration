3d50ae1c80208b56e69b44edc2079455686f6401
John T. White <john.t.white@gmail.com>
2016-11-19 18:05:49 -0500

    Added example.

---
+++ example/Example.Library/ExampleServiceConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using StaticDotNet.DependencyInjection.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Example.Library
+{
+	public class ExampleServiceConfiguration
+		: IServiceConfiguration
+	{
+		public void ConfigureServices( IServiceCollection services )
+		{
+			services.AddTransient<IExampleDependency, ExampleDependency>();
+		}
+	}
+}
---
+++ example/Example.WebApplication/Controllers/ExampleController.cs
@@ -0,0 +1,28 @@
+﻿using Example.Library;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Example.WebApplication.Controllers
+{
+	[Route( "Example" )]
+    public class ExampleController
+		: Controller
+    {
+		public ExampleController( IExampleDependency exampleDependency )
+		{
+			this.ExampleDependency = exampleDependency;
+		}
+
+		public IExampleDependency ExampleDependency { get; }
+
+		public IActionResult Index()
+		{
+			this.ViewBag.ExampleDependency = this.ExampleDependency;
+
+			return this.View();
+		}
+    }
+}
